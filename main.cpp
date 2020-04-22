11cc3c8552be27499915d994d6dc707b5995cb34
Shengyao Zhu <shengyao.zhu@gmail.com>
2020-04-22 13:47:05 +0200

    Initioal commit

---
+++ main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <sstream>
+#include <ctime>
+#include <random>
+#include <random>
+
+#include "./handin1/2_6.h"
+#include "./handin1/3_3.h"
+#include "./handin1/5_3.h"
+#include "./handin1/5_4.h"
+#include "./handin1/5_6.h"
+#include "./handin1/5_9.h"
+#include "./handin1/5_10.h"
+
+int main(int argc, char* argv[])
+{
+	double x = 0.03, y = 0.02 ;
+	std::cout << "x = " << x << " and y = " << y << "\n";
+  // 2.6 double newton_Raphson(double initialGuess, double epsilon);
+  //double newton = newton_Raphson(0,  0.0001);
+  //std::cout << newton <<"\n";
+
+  // 3.3
+  // std::cout << "euler\n";
+  // implicit_Euler(n);
+
+  // 5.3
+  // pointerfun();
+  
+  // 5.4
+  // mean();
+
+  // 5.6 Multiply matrix and vectors
+  // ...
+
+  return 0;
+}
